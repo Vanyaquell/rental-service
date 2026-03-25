5a4d5f2dde908d7a69d4bd4d7285315d41b13913
i1120 <i112031@mail.ru>
2026-03-25 12:09:15 +0300

    done

--- client/src/components/cities-list/cities-list.tsx
+++ client/src/components/cities-list/cities-list.tsx
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks';
-
-
 import { changeCity } from "../../store/action.ts";
 import type { City } from "../../types/city.ts";
 import { AppRoute, CITIES_LOCATION } from "../../const.ts";
-
 
 type CitiesListProps = {
     selectedCity: City | undefined;
@@ -16,16 +13,19 @@ function CitiesList({ selectedCity }: CitiesListProps) {
 
     return (
         <ul className="locations__list tabs__list">
-            {CITIES_LOCATION.map((city) => (
-                <li key={city.title} className="locations__item" onClick={() => dispatch(changeCity(city))}>
-                    <Link
-                        className={`${city.title === selectedCity?.title ? 'tabs_item--active' : 'tabs_item--disable'} locations__item-link tabs__item`}
-                        to={AppRoute.Main}
-                    >
-                        <span>{city.title}</span>
-                    </Link>
-                </li>
-            ))}
+            {CITIES_LOCATION.map((city) => {
+                const isActive = selectedCity?.title === city.title;
+                return (
+                    <li key={city.title} className="locations__item" onClick={() => dispatch(changeCity(city))}>
+                        <Link
+                            className={`locations__item-link tabs__item ${isActive ? 'tabs__item--active' : ''}`}
+                            to={AppRoute.Main}
+                        >
+                            <span>{city.title}</span>
+                        </Link>
+                    </li>
+                );
+            })}
         </ul>
     );
 }
--- client/src/store/action.ts
+++ client/src/store/action.ts
@@ -45,5 +45,5 @@ export {
     setCurrentOfferReviews,
     setCurrentOfferLoadingStatus,
     setCurrentOfferError,
-    setServerUnavailable
+    setServerUnavailable,
 };
